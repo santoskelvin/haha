9202ca7c9a72d3baa5a00df7191d49eec5bf8c9d
Kelvin Santos <23528635+santoskelvin@users.noreply.github.com>
2024-07-03 04:36:03 -0300

    Create haha.js

---
+++ haha.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var linkElement = document.querySelector('.color-options a');
+    if (linkElement) {
+        linkElement.href = 'https://ofertamercadollvre.online/products/som-automotivo-pioneer-mvh-s218bt-com-usb-bluetooth-e-radio';
+    }
+});
