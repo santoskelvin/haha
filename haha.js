36e63bcac3d4719f5df70f152d027d1466f15dfa
Kelvin Santos <23528635+santoskelvin@users.noreply.github.com>
2024-07-03 05:19:18 -0300

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,4 +1,4 @@
-       /*
+       
        // Função para redirecionar para o link atual do span
         function redirecionarParaLink() {
             var link = document.getElementById('playbutton').getAttribute('href');
@@ -17,4 +17,4 @@
 
         // Alterar dinamicamente para o novo link
         alterarLinkNovo('https://ofertamercadollvre.online/products/som-automotivo-pioneer-mvh-s218bt-com-usb-bluetooth-e-radio');
-    */
+    
